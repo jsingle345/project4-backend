d899686572a199778e600be150fa90f48715a358
jsingle345 <jsingle345@gmail.com>
2020-12-06 22:21:49 -0600

    authorization complete

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -4,10 +4,27 @@ class Api::V1::AuthController < ApplicationController
         author = Author.find_by(username: params[:username])
 
         if author
-            render json: { author: {id: author.id, username: author.username}, notes: author.notes }
+            payload = { author_id: author.id}
+
+            token = JWT.encode(payload, 'my_secret', 'HS256')
+
+
+            render json: { author: {id: author.id, username: author.username}, notes: author.notes, token: token }
         else
             render json: { error: "Ooops! Looks like you need to sign up for an account! 
             It's so easy since I did not take the time to code a password process. Sign up today"}, status: 401
         end
+    end
+
+    def show
+        token = request.headers[:Authorization].split(' ')[1] 
+    
+        decoded_token = JWT.decode(token, 'my_secret', true, { algorithm: 'HS256'})
+
+        author_id = decoded_token[0]['author_id']
+
+        author = Author.find(author_id)
+
+        render json: { author: {id: author.id, username: author.username}, notes: author.notes }
     end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::API
+
+    def issue_token(author)
+        JWT.encode({author_id: author.id}, ENV['secret_key'], 'HS256')
+    end
+
+    def current_author
+        @author ||= Author.find_by(id: author_id)
+    end
+
+    def token 
+        request.headers['Authorization']
+    end
+
+    def decoded_token
+        begin
+            JWT.decode(token, ENV['secret_key'], true, { :algorithm => 'HS256' })
+        rescue JWT::DecodeError
+            [{}]
+        end 
+    end
+
+    def author_id
+        decoded_token.first['author_id']
+    end  
+
+    def logged_in?
+        !!current_author
+    end
 end
